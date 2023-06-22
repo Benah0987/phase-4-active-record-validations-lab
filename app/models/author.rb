7fdde6ecebefd5086acd2cd6e7998dd77f4fbe00
Benah0987 <wanyoikebenayah77@gmail.com>
2023-06-22 16:27:30 +0300

    using custom methods

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    # adding validations
+    validates :name, presence: true
+    validates :name, uniquness: true
+    validates :phone_number, length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimumm: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+
+    # custom validation
+    validates :words
+
+    private
+#   function words
+    def words
+        # define the words
+        clickbait_keywords = ["Won't Believe", "Secret", "Top", "Guess"]
+        # what i awant to happen
+        unless clickbait_keywords.any? {|keyword| title.include?(keyword)}
+            errors.add(:title, "should be sufficiently clickbait-y")
+        end
+    end
+
 end
+
+
